d59482bcb2842ac88300a1b91981396607975a14
friedshrimpbbq <fried.shrimp34@yahoo.com>
2023-01-04 00:10:33 +1100

    Adding easyjson support

---
+++ schemas/go.mod
@@ -0,0 +1,8 @@
+module tinymlkit/schemas
+
+go 1.19
+
+require (
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+)
---
+++ schemas/linear_model/linear_model.go
@@ -0,0 +1,16 @@
+package linear_model_schema
+
+
+type LinearModelOptions struct {
+	Penalty string
+	Alpha float64
+	L1Ratio float64
+}
+
+type LinearModel struct {
+	Coefficient []float64
+	Intercept float64
+	Simplex [][]float64
+	SimplexLabels []float64
+	Options LinearModelOptions
+}
