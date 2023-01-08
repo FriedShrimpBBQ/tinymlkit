79e3dd8b5197ee20b1279a01d66710ea3ac6aee6
friedshrimpbbq <fried.shrimp34@yahoo.com>
2023-01-08 12:42:35 +1100

    implement inference functions

--- ml_model/linear_model.go
+++ ml_model/linear_model.go
@@ -6,6 +6,22 @@ import (
 	"ml_schemas"
 )
 
+type LinearModel struct {
+	lm ml_schemas.LinearModel
+}
+
+func (linear_model LinearModel) Predict(dataset ml_schemas.Dataset) []float64 {
+	predictions := []float64{}
+	for i := 0; i < len(dataset.Features); i++ {
+		var prediction float64 = linear_model.lm.Intercept
+		for j := 0; j < len(linear_model.lm.Coefficient); j++ {
+			prediction = prediction + linear_model.lm.Coefficient[j]*dataset.Features[i][j]
+		}
+		predictions = append(predictions, prediction)
+	}
+	return predictions
+}
+
 func LinearModelMapper(linearModelObj *ml_schemas.LinearModel) []float64 {
 	x := []float64{}
 	x = append(x, linearModelObj.Intercept)
--- ml_model/linear_model_test.go
+++ ml_model/linear_model_test.go
@@ -17,3 +17,17 @@ func TestLinearModelMapper(t *testing.T) {
 		t.Fatalf("intercept should 0th element")
 	}
 }
+
+func TestLinearModelPrediction(t *testing.T) {
+	sampleLinearModel := &ml_schemas.LinearModel{}
+	sampleLinearModel.UnmarshalJSON([]byte(`{"Coefficient": [1,2,3,4], "Intercept": 5, "Penalty": "l2", "Alpha": 6, "L1Ratio": 7}`))
+
+	dataset := [][]float64{
+		{1.0, 1.0, 1.0, 1.0},
+	}
+	output := LinearModel{*sampleLinearModel}.Predict(ml_schemas.Dataset{dataset})
+	expected := 15.0 // 1+2+3+4+5
+	if output[0] != expected {
+		t.Fatalf("Incorrect inference")
+	}
+}
