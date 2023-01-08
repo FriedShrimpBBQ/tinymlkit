2464891a5af1425deb6c61a97fb3950c986c352f
friedshrimpbbq <fried.shrimp34@yahoo.com>
2023-01-08 11:50:56 +1100

    patch

--- ml_model/linear_model.go
+++ ml_model/linear_model.go
@@ -6,7 +6,7 @@ import (
 	"ml_schemas"
 )
 
-func inearModelMapper(linearModelObj *ml_schemas.LinearModel) []float64 {
+func LinearModelMapper(linearModelObj *ml_schemas.LinearModel) []float64 {
 	x := []float64{}
 	x = append(x, linearModelObj.Intercept)
 	for i := 0; i < len(linearModelObj.Coefficient); i++ {
