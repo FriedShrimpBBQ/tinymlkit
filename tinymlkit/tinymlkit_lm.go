b342b2214b1cc5d73fdb19a42e5545cc01c0e5b4
friedshrimpbbq <fried.shrimp34@yahoo.com>
2023-01-09 22:49:06 +1100

    cleaning up the code to prepare for loading json datasets

--- tinymlkit/tinymlkit_lm.go
+++ tinymlkit/tinymlkit_lm.go
@@ -14,23 +14,18 @@ func main() {
 	penalty := "l2"
 	alpha := 0.0001
 	l1ratio := 0.15
-	
+
 	flag.StringVar(&penalty, "penalty", "l2", "Penalty for the linear model")
 	flag.Float64Var(&alpha, "alpha", 0.0001, "Regularizer weight")
 	flag.Float64Var(&l1ratio, "l1_ratio", 0.15, "l1 ratio if penalty is elasticnet")
 	flag.Parse()
-	
+
 	baseLM := ml_schemas.LinearModel{coefficient, intercept, penalty, alpha, l1ratio}
 
-	dataset := [][]float64{
-		{1.0, 1.0, 1.0, 1.0},
-	}
-	label := []float64{14.0}
-
-	lm := ml_model.LinearModel{baseLM}.SetWeightsAndCopy(ml_model.LinearModelInitWeights(len(dataset[0])))
-	labelledDataset := ml_schemas.LabelledDataset{dataset, label}
-	fmt.Println("Initial Loss\t", lm.MSELoss(labelledDataset))
+	labelledDataset := &ml_schemas.LabelledDataset{[][]float64{{1.0, 1.0, 1.0, 1.0}}, []float64{14.0}}
+	lm := ml_model.LinearModel{baseLM}.SetWeightsAndCopy(ml_model.LinearModelInitWeights(len(labelledDataset.Features[0])))
+	fmt.Println("Initial Loss\t", lm.MSELoss(*labelledDataset))
 	// train the model
-	x1 := optim.RandomOptim(ml_model.LinearModelMSEObjective(lm.Lm, labelledDataset), lm.LinearModelMapper(), 100)
-	fmt.Println("After Optim\t", lm.SetWeightsAndCopy(x1).MSELoss(labelledDataset))
+	x1 := optim.RandomOptim(ml_model.LinearModelMSEObjective(lm.Lm, *labelledDataset), lm.LinearModelMapper(), 100)
+	fmt.Println("After Optim\t", lm.SetWeightsAndCopy(x1).MSELoss(*labelledDataset))
 }
